678726780054b9e397af1f3f687f6b4edf60743e
jialong2.li <jialong2.li@ly.com>
2020-12-04 17:01:34 +0800

    init

---
+++ src/main/java/com/ly/mosn/MosnTestApplication.java
@@ -0,0 +1,13 @@
+package com.ly.mosn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosnTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosnTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ly/mosn/MosnTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.mosn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosnTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
